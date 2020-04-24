d897f8b168196c7c87fecda4d963300e2c9f38c8
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-24 00:43:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{holiday_caps}:"
       value.each do |key, value|
 
-          puts "/t#{key.to_s.capitalize!}: #{value.join(", ")}"
+          puts "    #{key.to_s.capitalize!}: #{value.join(", ")}"
       end
     end
 
